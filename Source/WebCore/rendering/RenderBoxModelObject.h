54b10abf74d24a2069adb5893cb3c363f213a951
Alan Baradlay <zalan@apple.com>
2026-09-12 06:16:36 -0700

    [cleanup] Compute boundingRects() once from each renderer's local border box rects https://bugs.webkit.org/show_bug.cgi?id=324042 <rdar://problem/187280208>
    Reviewed by NOBODY (OOPS!).
    
    1. localBorderBoxRects() on RenderBoxModelObject: each renderer reports the rects it encloses in its own coordinates: a box has one border box, an inline box has one per line, a line break has its line box.
    
    2. boundingRects() is written once: all it does is offset those rects by the caller's offset, so the copies in RenderBox, RenderBlock, RenderView, RenderLineBreak and RenderInline go away; RenderBlock's was identical to RenderBox's.
    (RenderInline::collectLineBoxRects only differed by skipping empty rects, which OutlinePainter now does itself instead of downcasting to RenderInline)
    
    * Source/WebCore/rendering/OutlinePainter.cpp:
    (WebCore::OutlinePainter::collectFocusRingRectsForInline):
    * Source/WebCore/rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::boundingRects): Deleted.
    * Source/WebCore/rendering/RenderBlock.h:
    * Source/WebCore/rendering/RenderBox.cpp:
    (WebCore::RenderBox::localBorderBoxRects const):
    (WebCore::RenderBox::boundingRects const): Deleted.
    * Source/WebCore/rendering/RenderBox.h:
    * Source/WebCore/rendering/RenderBoxModelObject.cpp:
    (WebCore::RenderBoxModelObject::localBorderBoxRects const):
    (WebCore::RenderBoxModelObject::boundingRects const):
    * Source/WebCore/rendering/RenderBoxModelObject.h:
    * Source/WebCore/rendering/RenderInline.cpp:
    (WebCore::RenderInline::absoluteQuads const):
    (WebCore::RenderInline::lineBoxRects const): Deleted.
    (WebCore::RenderInline::boundingRects const): Deleted.
    (WebCore::RenderInline::collectLineBoxRects const): Deleted.
    * Source/WebCore/rendering/RenderInline.h:
    * Source/WebCore/rendering/RenderLineBreak.cpp:
    (WebCore::RenderLineBreak::localBorderBoxRects const):
    (WebCore::RenderLineBreak::boundingRects const): Deleted.
    * Source/WebCore/rendering/RenderLineBreak.h:
    * Source/WebCore/rendering/RenderView.cpp:
    (WebCore::RenderView::localBorderBoxRects const):
    (WebCore::RenderView::boundingRects const): Deleted.
    * Source/WebCore/rendering/RenderView.h:

--- Source/WebCore/rendering/RenderBoxModelObject.h
+++ Source/WebCore/rendering/RenderBoxModelObject.h
@@ -96,6 +96,8 @@ public:
     virtual LayoutRect borderBoundingBox() const = 0;
 
     virtual LayoutRect NODELETE borderBoxRectInContainer() const;
+    virtual Vector<FloatRect> localBorderBoxRects() const;
+    void boundingRects(Vector<LayoutRect>&, const LayoutPoint& accumulatedOffset) const final;
 
     // These return the CSS computed padding values.
     inline LayoutUnit computedCSSPaddingTop() const;
