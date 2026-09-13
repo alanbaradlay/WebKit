ac3166525122c0a562e6ee8e2104deb78196a6a5
Alan Baradlay <zalan@apple.com>
2026-09-13 05:50:01 -0700

    [cleanup] Move computeVisibleRectsInContainer() up to RenderBoxModelObject https://bugs.webkit.org/show_bug.cgi?id=324101 <rdar://problem/187318089>
    Reviewed by NOBODY (OOPS!).
    
    RenderBox and RenderInline walked to the container the same way, so RenderBoxModelObject does it now and asks for a box only where one is needed: reflectedRect(), flipForWritingMode(), locationOffset() and the layer's transform.
    
    An inline box keeps VisibleRectContext::Option::ApplyCompositedContainerScrolls on the container clip. The paint offset cache's PositionType::Fixed check does not reach it since an out-of-flow position blockifies display.
    
    * Source/WebCore/rendering/RenderBox.cpp:
    (WebCore::RenderBox::computeVisibleRectsInContainer const): Deleted.
    * Source/WebCore/rendering/RenderBox.h:
    * Source/WebCore/rendering/RenderBoxModelObject.cpp:
    (WebCore::RenderBoxModelObject::computeVisibleRectsInContainer const):
    * Source/WebCore/rendering/RenderBoxModelObject.h:
    * Source/WebCore/rendering/RenderInline.cpp:
    (WebCore::RenderInline::computeVisibleRectsInContainer const): Deleted.
    * Source/WebCore/rendering/RenderInline.h:

--- Source/WebCore/rendering/RenderBox.h
+++ Source/WebCore/rendering/RenderBox.h
@@ -524,7 +524,6 @@ public:
     FloatRect objectBoundingBox() const override { return borderBoxRect(); }
 
     RepaintRects localRectsForRepaint(RepaintOutlineBounds) const override;
-    std::optional<RepaintRects> computeVisibleRectsInContainer(const RepaintRects&, const RenderLayerModelObject* container, const VisibleRectContext&, VisibleRectState) const override;
     void repaintDuringLayoutIfMoved(const LayoutRect&);
     virtual void repaintOverhangingFloats(bool paintAllDescendants);
 
@@ -711,7 +710,7 @@ private:
 
     LayoutRect frameRectForStickyPositioning() const override { return borderBoxRectInContainer(); }
 
-    RepaintRects computeVisibleRectsUsingPaintOffset(const RepaintRects&) const;
+    RepaintRects computeVisibleRectsUsingPaintOffset(const RepaintRects&) const final;
     
     LayoutPoint topLeftLocationWithFlipping() const;
 
--- Source/WebCore/rendering/RenderBoxModelObject.h
+++ Source/WebCore/rendering/RenderBoxModelObject.h
@@ -193,6 +193,7 @@ public:
     void mapAbsoluteToLocalPoint(OptionSet<MapCoordinatesMode>, TransformState&) const override;
 
     PositionWithAffinity positionForPoint(const LayoutPoint&, HitTestSource, const RenderFragmentContainer*) override;
+    std::optional<RepaintRects> computeVisibleRectsInContainer(const RepaintRects&, const RenderLayerModelObject* container, const VisibleRectContext&, VisibleRectState) const override;
 
     void setSelectionState(HighlightState) override;
 
@@ -239,7 +240,7 @@ protected:
 
 protected:
     const RenderElement* pushMappingToContainer(const RenderLayerModelObject* ancestorToStopAt, RenderGeometryMap&) const override;
-    RepaintRects computeVisibleRectsUsingPaintOffset(const RepaintRects&) const;
+    virtual RepaintRects computeVisibleRectsUsingPaintOffset(const RepaintRects&) const;
 
 private:
     virtual LayoutRect frameRectForStickyPositioning() const = 0;
